99baae22b0b1cae037628680b0d38e678786e037
Diana Arango <dianaarango@dianas-mbp.lan>
2021-10-18 23:01:59 -0400

    add code

---
+++ Mapping_Lines/js/logic.js
@@ -0,0 +1,18 @@
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid'). setView([36.1733, -120.1794], 7);
+  
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+// Create a polyline using the line coordinates and make the line black.
+L.polyline(line, {
+    color: "yellow"
+ }).addTo(map);
+
+ // Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
